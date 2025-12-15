470e98bf0f82651ec46992f76fc1b0ee5abfcc35
Magio-ore <74238314+Magio-ore@users.noreply.github.com>
2025-12-16 01:50:25 +0200

    Enhance About Us section styles and update Swiper configuration for better functionality and layout

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,12 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-    speed: 600,
-    parallax: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
