8bd6f3bbf768ceea3736f0ad800fe1b6012627c3
Irina Zaitseva <ira.zajtseva89@gmail.com>
2025-12-15 19:24:33 +0200

    modal-order

---
+++ src/js/modal-order.js
@@ -0,0 +1,8 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const backdrop = document.querySelector('[data-modal-order]');
+const orderCloseBtn = document.querySelector('[data-modal-order-close]');
+const form = document.querySelector('[data-modal-order-form]');
+
+let currentPetId = null;
